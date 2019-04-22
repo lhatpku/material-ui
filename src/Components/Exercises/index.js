30f18c218086991e492a02024cf03dfdfd368228
Hao Lu <haolu@Haos-MacBook-Pro.local>
2019-04-21 21:47:42 -0500

    material-ui, day1

---
+++ src/Components/Exercises/index.js
@@ -0,0 +1,69 @@
+import React , {Fragment } from 'react'
+import {Grid, Paper, Typography, List, ListItem, ListItemText} from '@material-ui/core'
+
+import RightPane from './RightPane'
+
+const styles = {
+
+    Paper: {padding: 20, marginTop:10, marginBottom: 10, height: 500, overflowY: 'auto'}
+
+}
+
+export default ({
+    exercises, 
+    category, 
+    onSelect, 
+    exercise: {
+        id, 
+        title = 'Welcome!', 
+        description = 'Please select an exercise from the list on the left.'
+    }
+}) =>
+    <Grid container>
+        <Grid item >
+            <Paper style={styles.Paper}>
+                {exercises.map(([group, exercises]) =>
+                    !category || category === group
+                        ? <Fragment key = {group}>
+                            <Typography 
+                                variant = "headline"
+                                style = {{textTransform: 'capitalize'}}
+                            >
+                                {group}
+                            </Typography>
+                            <List component="ul">
+                                {exercises.map(({id, title}) => 
+                                    <ListItem button
+                                            key = {id}
+                                            primary={title} 
+                                            onClick={() => onSelect(id)}
+                                    >
+                                        <ListItemText primary = {title}/>
+                                    </ListItem>
+                                )}
+                            </List>
+                        </Fragment>     
+                        : null      
+                )}
+            </Paper>
+        </Grid>
+        <Grid item>
+            <Paper style={styles.Paper}>
+                <Typography
+                    variant = "display1"
+                >
+                   {title}
+                </Typography>
+                <Typography
+                    variant = "subheading"
+                    style = {{marginTop: 20}}
+                >
+                    {description}
+                </Typography>
+
+
+            </Paper>
+        </Grid>
+    </Grid>
+
+     
---
+++ src/Components/Layouts/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {AppBar, Toolbar, Typography} from '@material-ui/core'
+
+
+export default props => 
+    <AppBar position="static">
+        <Toolbar>
+            <Typography variant="headline" color="inherit">
+                News
+            </Typography>
+        </Toolbar>
+    </AppBar>
+
